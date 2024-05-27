ef378efed79046a0cffd7d772a49664fb3c82725
Leonardo Santiago <leonardo201220@gmail.com>
2024-05-27 09:44:05 -0300

    Formulário de cadastro

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+//const bootstrap = require("bootstrap")
 
 
 app.use(express.static("public"));
+app.use(express.static("./node_modules/bootstrap/dist/css"))
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
 
-mongoose.connect("mongodb://localhost:27017/agendamento", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://127.0.0.1/agendamento");
 
 app.get("/", (req, res) => {
     res.send("Oi!")
 });
+
+app.get("/cadastro", (req, res) => {
+    res.render("create");
+})
 
 app.listen(8080, () => {});
